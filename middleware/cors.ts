e3ffa95fe635d10ada6eda36f1f49faeae7a048d
Lucky-kor <jungmin2056@gmail.com>
2021-05-17 18:59:33 +0900

    [Create] setup router connection

--- middleware/cors.ts
+++ middleware/cors.ts
@@ -1,1 +1,8 @@
-import cors from "cors";
+const corsOption = {
+  origin: ['https://localhost:3000', "https://pikaff.ga", "https://www.pikaff.ga"] ,
+  methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT'],
+  optionsSuccessStatus: 200,
+  credentials: true,
+}
+
+export default corsOption;
--- server/index.ts
+++ server/index.ts
@@ -1,30 +1,37 @@
 // basic server modules.
 import express, { Request, Response } from "express";
-// import { cors } from "../middleware/cors"; cors 미들웨어 작성 후 주석해제.
+import corsOption from "../middleware/cors";
+import cors from 'cors'
 import bodyParser from "body-parser";
+import router from "../router/index";
 // typeorm
 import { createConnection, Connection } from "typeorm";
 // etc
 import "dotenv";
+import app from "./server"
 
 // entity connction
-createConnection()
-  .then(async (connection: Connection) =>
-    console.log("Entity connected : ", connection.isConnected)
-  )
-  .catch((err) => {
-    console.log("Entity conncetion error : ", err);
-  });
+// createConnection()
+//   .then(async (connection: Connection) =>
+//     console.log("Entity connected : ", connection.isConnected)
+//   )
+//   .catch((err) => {
+//     console.log("Entity conncetion error : ", err);
+//   });
 
 // create express server
-const app = express();
+// const app = express();
+
+// app.use(cors(corsOption));
+
+// //router setup
+// app.use('/', router);
 
 //setting port
-app.set("port", 3000);
-
+app.set("port", 3306);
 const server = app.listen(app.get("port"), () => {
   console.log(`App is listening on PORT ${app.get("port")} `);
 });
 
 // 서버 내용 작성.
-export default server;
+// export default server;
