38c23b2444804b79ecc1bb242a759da96dc3b49a
Lucky-kor <jungmin2056@gmail.com>
2021-05-19 16:03:21 +0900

    [Create] Signup

---
+++ controllers/user/userHandler.ts
@@ -0,0 +1,4 @@
+export { default as signup } from "./signup"
+export { default as mail } from "./mail"
+// export { default as signin } from "./signin"
+// export { default as signout } from "./signout"
--- router/userRouter.ts
+++ router/userRouter.ts
@@ -1,7 +1,16 @@
 import express from "express";
-import { userController } from "@controllers/index";
+import { userController } from '../controllers/index';
 const router = express.Router();
 
+router.post('/signup', userController.signup);
+// router.post('/google', userController.google);
+// router.post('/kakao', userController.kakao);
+// router.post('/signin', userController.signin);
+// router.post('/signout', userController.signout);
+// router.delete('/', userController.signoff);
+// router.patch('/', userController.modification);
+// router.patch('/test', userController.addtest);
+// router.get('/', userController.userinfo);
 router.post("/email", userController.mail);
-router.get("/", userController.get);
+
 export default router;
