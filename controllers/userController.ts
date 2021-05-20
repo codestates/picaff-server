a80db4bb087dc571ad93cef99f4e51c47ca32c71
Byron Kim <bironkim88@gmail.com>
2021-05-20 16:50:03 +0900

    re_google Oauth done ag

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,5 +1,5 @@
-import { Response, Request } from "express";
-import { signIn, signUp, mail, googleOauth } from "./user/userHandler";
+import { Response, Request } from 'express';
+import { signIn, signUp, mail, googleOauth } from './user/userHandler';
 
 export default {
   signUp,
