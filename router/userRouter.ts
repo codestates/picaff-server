55df9eeaedf8bc8f673c570ba71cabcd6b99a1ee
Byron Kim <bironkim88@gmail.com>
2021-05-20 16:43:40 +0900

    singleQuote fixed

--- router/userRouter.ts
+++ router/userRouter.ts
@@ -1,16 +1,16 @@
-import express from "express";
-import { userController } from "../controllers/index";
+import express from 'express';
+import { userController } from '../controllers/index';
 const router = express.Router();
 
-router.post("/signup", userController.signUp);
-router.post("/google", userController.googleOauth);
+router.post('/signup', userController.signUp);
+router.post('/google', userController.googleOauth);
 // router.post('/kakao', userController.kakao);
-router.post("/signin", userController.signIn);
+router.post('/signin', userController.signIn);
 // router.post('/signout', userController.signOut);
 // router.delete('/', userController.signOff);
 // router.patch('/', userController.modification);
 // router.patch('/test', userController.addTest);
 // router.get('/', userController.userInfo);
-router.post("/email", userController.mail);
+router.post('/email', userController.mail);
 
 export default router;
