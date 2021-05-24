b447f907e5d508137c528d8d58b82c2cd4531f7e
Byron Kim <bironkim88@gmail.com>
2021-05-24 17:29:25 +0900

    err fixed

--- controllers/user/kakaoOauth.ts
+++ controllers/user/kakaoOauth.ts
@@ -7,70 +7,74 @@ import { kakaoProperties } from '@interface/type'
 const kakaoOauth = async (req: Request, res: Response) => {
   const access_token: string = req.body.access_token
 
-  const verifyTokenInfo = await axios({
-    method: 'POST',
-    url: 'https://kapi.kakao.com/v2/user/me',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'Authorization': `Bearer ${access_token}`,
-    },
-  })
+  try {
+    const verifyTokenInfo = await axios({
+      method: 'POST',
+      url: 'https://kapi.kakao.com/v2/user/me',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Authorization': `Bearer ${access_token}`,
+      },
+    })
 
-  const id: string = verifyTokenInfo.data.id
-  const properties: kakaoProperties = verifyTokenInfo.data.properties
+    const id: string = verifyTokenInfo.data.id
+    const properties: kakaoProperties = verifyTokenInfo.data.properties
 
-  if (verifyTokenInfo.status === 200) {
-    const checkUser = await interfaces.getKakaoUserInfo('kakaoUser' + id)
-    console.log(checkUser)
-    if (checkUser) {
-      const accessToken = token.generateAccessToken(
-        checkUser.id,
-        checkUser.email,
-        checkUser.userName
-      )
-      const refreshToken = token.generateRefreshToken(
-        checkUser.id,
-        checkUser.email,
-        checkUser.userName
-      )
-      res
-        .status(200)
-        .cookie('refreshToken', refreshToken, { httpOnly: true })
-        .send({
-          id: checkUser.id,
-          userName: checkUser.userName,
-          email: checkUser.email,
-          auth: {
-            accessToken: accessToken,
-          },
-        })
-    } else {
-      await interfaces.createUser('kakaoUser' + `${id}`, properties.nickname, 'kakaoOauth')
-      const oauthUserInfo = await interfaces.getKakaoUserInfo('kakaoUser' + id)
-      if (oauthUserInfo !== undefined) {
+    if (verifyTokenInfo.status === 200) {
+      const checkUser = await interfaces.getKakaoUserInfo('kakaoUser' + id)
+      console.log(checkUser)
+      if (checkUser) {
         const accessToken = token.generateAccessToken(
-          oauthUserInfo.id,
-          oauthUserInfo.email,
-          oauthUserInfo.userName
+          checkUser.id,
+          checkUser.email,
+          checkUser.userName
         )
         const refreshToken = token.generateRefreshToken(
-          oauthUserInfo.id,
-          oauthUserInfo.email,
-          oauthUserInfo.userName
+          checkUser.id,
+          checkUser.email,
+          checkUser.userName
         )
         res
-          .status(201)
+          .status(200)
           .cookie('refreshToken', refreshToken, { httpOnly: true })
           .send({
-            id: oauthUserInfo.id,
-            userName: oauthUserInfo.userName,
-            email: oauthUserInfo.email,
+            id: checkUser.id,
+            userName: checkUser.userName,
+            email: checkUser.email,
             auth: {
               accessToken: accessToken,
             },
           })
+      } else {
+        await interfaces.createUser('kakaoUser' + `${id}`, properties.nickname, 'kakaoOauth')
+        const oauthUserInfo = await interfaces.getKakaoUserInfo('kakaoUser' + id)
+        if (oauthUserInfo !== undefined) {
+          const accessToken = token.generateAccessToken(
+            oauthUserInfo.id,
+            oauthUserInfo.email,
+            oauthUserInfo.userName
+          )
+          const refreshToken = token.generateRefreshToken(
+            oauthUserInfo.id,
+            oauthUserInfo.email,
+            oauthUserInfo.userName
+          )
+          res
+            .status(201)
+            .cookie('refreshToken', refreshToken, { httpOnly: true })
+            .send({
+              id: oauthUserInfo.id,
+              userName: oauthUserInfo.userName,
+              email: oauthUserInfo.email,
+              auth: {
+                accessToken: accessToken,
+              },
+            })
+        }
       }
     }
+  } catch (err) {
+    res.status(400).send('Invalid token')
   }
 }
 
