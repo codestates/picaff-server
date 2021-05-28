41f3ad0b0bfd0de5b3a67063383b2b4f70cbb64b
Lucky-kor <jungmin2056@gmail.com>
2021-05-28 15:27:59 +0900

    [Update] seed data

--- src/seeds/testResult.seed.ts
+++ src/seeds/testResult.seed.ts
@@ -1,22 +1,57 @@
 export const TestResultDataSeed = [
   {
     userId: 1,
-    itemTypeId: 1,
+    itemTypeId: 6,
     coffeeTypeId: 1,
   },
   {
     userId: 2,
-    itemTypeId: 1,
-    coffeeTypeId: 1,
+    itemTypeId: 7,
+    coffeeTypeId: 3,
   },
   {
     userId: 3,
-    itemTypeId: 1,
+    itemTypeId: 8,
+    coffeeTypeId: 2,
+  },
+  {
+    userId: 4,
+    itemTypeId: 9,
+    coffeeTypeId: 4,
+  },
+  {
+    userId: 5,
+    itemTypeId: 6,
+    coffeeTypeId: 5,
+  },
+  {
+    userId: 1,
+    itemTypeId: 7,
+    coffeeTypeId: 2,
+  },
+  {
+    userId: 2,
+    itemTypeId: 6,
+    coffeeTypeId: 4,
+  },
+  {
+    userId: 3,
+    itemTypeId: 9,
     coffeeTypeId: 1,
   },
   {
     userId: 4,
-    itemTypeId: 1,
-    coffeeTypeId: 1,
+    itemTypeId: 8,
+    coffeeTypeId: 5,
+  },
+  {
+    userId: 5,
+    itemTypeId: 9,
+    coffeeTypeId: 3,
+  },
+  {
+    userId: null,
+    itemTypeId: 9,
+    coffeeTypeId: 3,
   },
 ];
