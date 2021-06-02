0eb14e0ab36243fbaa04ec6f3efc322dc5870ecf
Byron Kim <bironkim88@gmail.com>
2021-06-02 18:00:28 +0900

    after merged

--- router/itemRouter.ts
+++ router/itemRouter.ts
@@ -8,10 +8,6 @@ router.get('/all', itemController.getAllItems)
 router.post('/sharing', itemController.sharing)
 router.post('/liked', itemController.addLiked)
 router.get('/tag', itemController.getTag)
-<<<<<<< HEAD
-router.post('/price', itemController.priceComparison)
-=======
 router.post('/crawling', itemController.crawling)
 
->>>>>>> 498394a18267d5ffddf196fc433c856a808b47bc
 export default router
