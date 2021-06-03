75db21482d3916a0e88ef766f9888be9cd407c4a
Lucky-kor <jungmin2056@gmail.com>
2021-06-03 18:41:19 +0900

    [Update] itemDetail add '/'

--- src/seeds/item.seed.ts
+++ src/seeds/item.seed.ts
@@ -2,10 +2,9 @@ export const ItemDataSeed = [
   {
     itemName: '캐냐 AA',
     itemPrice: 18900,
-    itemDetail:
-      '<h3>깔끔하고 과일향의 산미로 새콤달콤한 목넘김이 좋은 커피</h3></br><p>케냐는 아라비카의 원산국인 에티오피아와 인접한 국가로 아프리카에서 고급커피를 생산하는 국가중 하나입니다.</br>높은 고도와 영양분이 가득한 토양, 풍부한 강수량등 기후 조건이 커피 재배에 적합하여 양질의 커피가 생산되고 있습니다.</br>SL28,Ruiru11 품종은 높은 품질을 가진 종으로 콩의 크기가 크고, 질병과 녹병의 저항성을 가지고 있습니다.</br>케냐에서 생산되는 커피는 대부분 수작업으로 수확하여 워시드 방식으로 가공합니다.</br>시트러스한 산미와 단 맛의 밸런스가 좋으며, 풍부한 아로마를 가진 특징이 있습니다</br></p>',
+    itemDetail:'깔끔하고 과일향의 산미로 새콤달콤한 목넘김이 좋은 커피/케냐는 아라비카의 원산국인 에티오피아와 인접한 국가로 아프리카에서 고급커피를 생산하는 국가중 하나입니다./높은 고도와 영양분이 가득한 토양, 풍부한 강수량등 기후 조건이 커피 재배에 적합하여 양질의 커피가 생산되고 있습니다/SL28,Ruiru11 품종은 높은 품질을 가진 종으로 콩의 크기가 크고, 질병과 녹병의 저항성을 가지고 있습니다./케냐에서 생산되는 커피는 대부분 수작업으로 수확하여 워시드 방식으로 가공합니다.</br>시트러스한 산미와 단 맛의 밸런스가 좋으며, 풍부한 아로마를 가진 특징이 있습니다',
     type: 'coffee',
-    imageUrl: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/kenya.png',
+    imageURL: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/kenya.png',
     iso: 'KE',
     productCharacterId: null,
     coffeeCharacterId: 1,
@@ -13,10 +12,9 @@ export const ItemDataSeed = [
   {
     itemName: '콜롬비아 수프리모',
     itemPrice: 16000,
-    itemDetail:
-      '<h3>풍부한 바디감과 목 넘김에 고소한 산미가 느껴지는 커피</h3></br><p>콜롬비아는 지리적으로 카리브해와 태평양을 접하고 있으며, 안데스 산맥을 끼고 있습니다.</br>안데스 산맥 지역은 비옥한 화산재와 커피 재배에 이상적인 기후 조건을 갖추고 있습니다.</br>콜롬비아의 커피는 Typica종으로 콩의 모양이 길고 둥글며 얇은 형태입니다.</br>가공은 워시드 방식으로 외관은 녹색을 띠고 은피가 적은것이 특징입니다.</br>일반적으로 분리, 펄핑, 점액질 제거, 세척 건조의 과정을 거치게 됩니다.</br>뛰어난 향과 산미를 가지며, 무거운 질감에 바디감이 강하여 견과류, 초콜릿, 캐러멜등의 향미를 느낄 수 있는 커피입니다.</br></p>',
+    itemDetail:'풍부한 바디감과 목 넘김에 고소한 산미가 느껴지는 커피/콜롬비아는 지리적으로 카리브해와 태평양을 접하고 있으며, 안데스 산맥을 끼고 있습니다./안데스 산맥 지역은 비옥한 화산재와 커피 재배에 이상적인 기후 조건을 갖추고 있습니다./콜롬비아의 커피는 Typica종으로 콩의 모양이 길고 둥글며 얇은 형태입니다./가공은 워시드 방식으로 외관은 녹색을 띠고 은피가 적은것이 특징입니다./일반적으로 분리, 펄핑, 점액질 제거, 세척 건조의 과정을 거치게 됩니다./뛰어난 향과 산미를 가지며, 무거운 질감에 바디감이 강하여 견과류, 초콜릿, 캐러멜등의 향미를 느낄 수 있는 커피입니다.',
     type: 'coffee',
-    imageUrl: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/colombia.png',
+    imageURL: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/colombia.png',
     iso: 'CO',
     productCharacterId: null,
     coffeeCharacterId: 2,
@@ -24,10 +22,9 @@ export const ItemDataSeed = [
   {
     itemName: '브라질 산토스',
     itemPrice: 16500,
-    itemDetail:
-      '<h3>부드럽고 향이 풍부하며 묵직한 바디감을 즐길 수 있는 커피</h3></br><p>브라질은 세계 최대 규모의 커피 생산국이며, 대규모 농장에서 경작하는 경우가 많습니다.</br>약한 산미와 비교적 묵직한 바디감으로 블랜딩커피의 베이스로도 자주 사용되며, 신맛을 싫어하시는 분들이 많이 찾으시는 커피입니다.</br>브라질 고유의 Pulped-natural 가공방식과 동글동글하고 납작한 콩의 외관이 브라질 커피의 또다른 특징입니다.</br></p>',
+    itemDetail:'부드럽고 향이 풍부하며 묵직한 바디감을 즐길 수 있는 커피/브라질은 세계 최대 규모의 커피 생산국이며, 대규모 농장에서 경작하는 경우가 많습니다./약한 산미와 비교적 묵직한 바디감으로 블랜딩커피의 베이스로도 자주 사용되며, 신맛을 싫어하시는 분들이 많이 찾으시는 커피입니다./브라질 고유의 Pulped-natural 가공방식과 동글동글하고 납작한 콩의 외관이 브라질 커피의 또다른 특징입니다.',
     type: 'coffee',
-    imageUrl: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/ethiopia.jpg',
+    imageURL: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/ethiopia.jpg',
     iso: 'BR',
     productCharacterId: null,
     coffeeCharacterId: 3,
@@ -35,10 +32,9 @@ export const ItemDataSeed = [
   {
     itemName: '과테말라 안티구아 SHB',
     itemPrice: 19000,
-    itemDetail:
-      '<h3>달콤함과 고소함, 거친듯한 향이 길게 여운을 남기는 커피</h3></br><p>과테말라 안티구아 지역은 세계적으로 훌륭한 커피가 생각되는 곳으로 유명합니다.</br>화산에 둘러싸인 자연조건 때문에 안티구아 지역은 유기물이 풍부한 토양을 지녔습니다.</br>과테말라 특유의 묵직함과 초콜릿같은 풍미가 특징입니다.</br>주로 습식법(Washed)방법을 사용하여 가공되어 아직 전통적인 방법으로 가공되는것도 과테말라 안티구아 커피만의 또다른 특징입니다.</br></p>',
+    itemDetail:'달콤함과 고소함, 거친듯한 향이 길게 여운을 남기는 커피/과테말라 안티구아 지역은 세계적으로 훌륭한 커피가 생각되는 곳으로 유명합니다./화산에 둘러싸인 자연조건 때문에 안티구아 지역은 유기물이 풍부한 토양을 지녔습니다./과테말라 특유의 묵직함과 초콜릿같은 풍미가 특징입니다./주로 습식법(Washed)방법을 사용하여 가공되어 아직 전통적인 방법으로 가공되는것도 과테말라 안티구아 커피만의 또다른 특징입니다.',
     type: 'coffee',
-    imageUrl: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/guatemala.png',
+    imageURL: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/guatemala.png',
     iso: 'GT',
     productCharacterId: null,
     coffeeCharacterId: 1,
@@ -46,10 +42,9 @@ export const ItemDataSeed = [
   {
     itemName: '에티오피아 예가체프 G2',
     itemPrice: 19000,
-    itemDetail:
-      '<h3>열대과일의 산미와 달콤이 여운으로 이어지는 커피</h3></br><p>에티오피아는 아프리카 최대의 커피 생산국이며, 아바리카 원종이 시작된 국가이기도 합니다.</br>지역마다 다양한 방식으로 재배되는 에티오피아 커피는 모카향으로 특정되는 독특한 향이 특징이며, 다른 어떤 종류의 커피와도 확연하게 구분되는 강한 개성을 가지고 있습니다.</br>그 중에서도 예가체프는 다채로운 꽃향기, 잘 익은 과일의 달콤한맛, 날카롭지 않은 산미와 부드러운 바디감이 특징이자 장점입니다.</br></p>',
+    itemDetail:'열대과일의 산미와 달콤이 여운으로 이어지는 커피/에티오피아는 아프리카 최대의 커피 생산국이며, 아바리카 원종이 시작된 국가이기도 합니다./지역마다 다양한 방식으로 재배되는 에티오피아 커피는 모카향으로 특정되는 독특한 향이 특징이며, 다른 어떤 종류의 커피와도 확연하게 구분되는 강한 개성을 가지고 있습니다./그 중에서도 예가체프는 다채로운 꽃향기, 잘 익은 과일의 달콤한맛, 날카롭지 않은 산미와 부드러운 바디감이 특징이자 장점입니다.',
     type: 'coffee',
-    imageUrl: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/ethiopia.jpg',
+    imageURL: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/ethiopia.jpg',
     iso: 'ET',
     productCharacterId: null,
     coffeeCharacterId: 5,
@@ -57,10 +52,9 @@ export const ItemDataSeed = [
   {
     itemName: '모카포트',
     itemPrice: 37000,
-    itemDetail:
-      '<h3>이탈리아의 국민 아이템 모카포트</h3></br><p>에스프레소를 추출할 수 있는 일종의 주전자입니다.</br>에스프레소 머신에 비해 가격이 저렴하여, 홈카페를 즐길때 자주 사용됩니다.</br>이탈리아에서는 가정집의 90%가 사용할 정도로 매우 인기가 많습니다.</br>보통 집에서 좋은 품질의 커피를 즐기고 싶을때, 에스프레소 머신의 가격이 부담될 때, 핸드드립과 모카포트 사이에서 저렴한 방법으로 에스프레소를 즐기고 싶을때, 모카포트를 선택하는 경우가 많습니다.</br>알류미늄 제품이 가장 유명하며, 스테인레스나 도자기로 만들어진 제품도 있습니다. 보통 세제의 사용없이 물로만 세척하며, 항상 깔끔한 맛의 커피를 추출 할 수 있지만, 관리가 조금 까다로운 것이 특징입니다.</br></p>',
+    itemDetail:'이탈리아의 국민 아이템 모카포트/에스프레소를 추출할 수 있는 일종의 주전자입니다./에스프레소 머신에 비해 가격이 저렴하여, 홈카페를 즐길때 자주 사용됩니다./이탈리아에서는 가정집의 90%가 사용할 정도로 매우 인기가 많습니다./보통 집에서 좋은 품질의 커피를 즐기고 싶을때, 에스프레소 머신의 가격이 부담될 때,/핸드드립과 모카포트 사이에서 저렴한 방법으로 에스프레소를 즐기고 싶을때, 모카포트를 선택하는 경우가 많습니다./알류미늄 제품이 가장 유명하며, 스테인레스나 도자기로 만들어진 제품도 있습니다./보통 세제의 사용없이 물로만 세척하며, 항상 깔끔한 맛의 커피를 추출 할 수 있지만, 관리가 조금 까다로운 것이 특징입니다.',
     type: 'product',
-    imageUrl: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/moka-pot+ori.jpg',
+    imageURL: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/moka-pot+ori.jpg',
     iso: '',
     productCharacterId: 1,
     coffeeCharacterId: null,
@@ -68,10 +62,9 @@ export const ItemDataSeed = [
   {
     itemName: '핸드드립',
     itemPrice: 40000,
-    itemDetail:
-      '<h3>홈카페의 시작, 핸드드립</h3></br><p>다른 커피와 달리 사람의 손으로 직접 물을 조절해가며 추출하며 그에따른 사용자의 숙련도에 따라 커피맛이 좌우됩니다.</br>프렌치프레소나 모카포트, 워터드립등에 비해서 우려내는 과정이 멋진 편이라 일본에서는 다도문화에 영향을 받아 많은 인기를 끌고 있습니다.</br>덕분에 한국에서도 핸드드립에 대한 정보나 용품등을 쉽게 구할 수 있고, 입문자도 늘어나는 추세입니다.</br>개개인의 역량에 따라 커피의 맛이 달리지기 때문에, 바리스타의 실력이 바로 드러나는 추출 방식입니다.</br></p>',
+    itemDetail:'홈카페의 시작, 핸드드립/다른 커피와 달리 사람의 손으로 직접 물을 조절해가며 추출하며 그에따른 사용자의 숙련도에 따라 커피맛이 좌우됩니다./프렌치프레소나 모카포트, 워터드립등에 비해서 우려내는 과정이 멋진 편이라 일본에서는 다도문화에 영향을 받아 많은 인기를 끌고 있습니다./덕분에 한국에서도 핸드드립에 대한 정보나 용품등을 쉽게 구할 수 있고, 입문자도 늘어나는 추세입니다./개개인의 역량에 따라 커피의 맛이 달리지기 때문에, 바리스타의 실력이 바로 드러나는 추출 방식입니다.',
     type: 'product',
-    imageUrl: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/hand-drip.jpg',
+    imageURL: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/hand-drip.jpg',
     iso: '',
     productCharacterId: 2,
     coffeeCharacterId: null,
@@ -79,10 +72,9 @@ export const ItemDataSeed = [
   {
     itemName: '캡슐커피머신',
     itemPrice: 55000,
-    itemDetail:
-      '<h3>빠르고 간단한 커피한잔, 캡슐커피</h3></br><p>캡슐커피는 일반 추출법과 다르게 머신에서 사용 가능한 캡슐을 넣은 후 버튼만 눌러주면 에스프레소가 추출됩니다.</br>항상 일정한 품질의 에스프레소 추출이 가능하며, 캡슐은 원두와 다르게 상온에 보관이 가능한 장점이 있습니다.</br>상대적으로 에스프레소 머신이나, 핸드드립을 사용 할 때 사용되는 그라인더나 여러가지 용품의 가격을 비교하면 저렴한 편에 속합니다.</br>그러나 개별 캡슐의 가격이 조금 비싼편으로, 커피를 자주 마실수록 한 잔에 대한 금액이 부담 될 수 있습니다.</br></p>',
+    itemDetail:'빠르고 간단한 커피한잔, 캡슐커피/캡슐커피는 일반 추출법과 다르게 머신에서 사용 가능한 캡슐을 넣은 후 버튼만 눌러주면 에스프레소가 추출됩니다./항상 일정한 품질의 에스프레소 추출이 가능하며, 캡슐은 원두와 다르게 상온에 보관이 가능한 장점이 있습니다./상대적으로 에스프레소 머신이나, 핸드드립을 사용 할 때 사용되는 그라인더나 여러가지 용품의 가격을 비교하면 저렴한 편에 속합니다./그러나 개별 캡슐의 가격이 조금 비싼편으로, 커피를 자주 마실수록 한 잔에 대한 금액이 부담 될 수 있습니다.',
     type: 'product',
-    imageUrl: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/capsule-machine.jpg',
+    imageURL: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/capsule-machine.jpg',
     iso: '',
     productCharacterId: 3,
     coffeeCharacterId: null,
@@ -90,10 +82,9 @@ export const ItemDataSeed = [
   {
     itemName: '워터드립',
     itemPrice: 80000,
-    itemDetail:
-      '<h3>찬물로 추출하는 커피, 콜드브루</h3></br><p>일반적인 커피는 뜨거운 물을 분쇄한 원두에 부어서 단기간에 추출하는 반면에, 워터드립은 차가운 물에 커피를 우려냅니다.</br>이 때, 찬물은 잘 우러나지 않기 때문에, 물을 한두방울씩 떨어트려 8~12시간에 걸려 추출합니다.</br>그 때문인지 천사의 눈물 이라고 불리기도 합니다.</br>이렇게 추출된 콜드브루는 무척 진하면서도 일반적인 드립 기구로는 맛볼수 없는 특유의 부드러운 질감과 단맛이 두드러지는 풍미가 특징입니다.</br></p>',
+    itemDetail:'찬물로 추출하는 커피, 콜드브루/일반적인 커피는 뜨거운 물을 분쇄한 원두에 부어서 단기간에 추출하는 반면에, 워터드립은 차가운 물에 커피를 우려냅니다./이 때, 찬물은 잘 우러나지 않기 때문에, 물을 한두방울씩 떨어트려 8~12시간에 걸려 추출합니다./그 때문인지 천사의 눈물 이라고 불리기도 합니다./이렇게 추출된 콜드브루는 무척 진하면서도 일반적인 드립 기구로는 맛볼수 없는 특유의 부드러운 질감과 단맛이 두드러지는 풍미가 특징입니다.',
     type: 'product',
-    imageUrl: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/water-drip.jpg',
+    imageURL: 'https://picaff-client-image.s3.ap-northeast-2.amazonaws.com/water-drip.jpg',
     iso: '',
     productCharacterId: 4,
     coffeeCharacterId: null,
